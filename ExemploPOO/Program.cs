76c11f90c59394feca59a08fc280a12f016fad69
Marcos Leite <m.abner@live.co.uk>
2022-02-22 13:22:56 -0300

    initial commit

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pessoa p1 = new Pessoa();
+
+			p1.Nome = "Marcos";
+			p1.Idade = 34;
+
+			p1.Apresentar();
+		}
+	}
+}
