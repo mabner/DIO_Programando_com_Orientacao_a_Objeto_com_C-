7d24aeecc1bc6bfb2c4ba0553e97db66eaefe2eb
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-02-23 13:24:08 -0300

    aluno and professor run test

--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -6,13 +6,27 @@ namespace ExemploPOO
 	{
 		static void Main(string[] args)
 		{
-			Retangulo ret1 = new Retangulo();
-			ret1.DefinirMedidas(30, 30);
-			System.Console.WriteLine($"Area: {ret1.ObterArea()}");
+			Aluno a1 = new Aluno();
+			a1.Nome = 'Marcos';
+			a1.Idade = 34;
+			a1.Nota = 10;
+			
+			a1.Apresentar();
 
-			Retangulo ret2 = new Retangulo();
-			ret2.DefinirMedidas(-10, 0);
-			System.Console.WriteLine($"Area: {ret2.ObterArea()}");
+			Professor p1 = new Professor();
+			p1.Nome = 'Leidiane';
+			p1.Idade = 34;
+			p1.Salario = 16000;
+			
+			p1.Apresentar();
+
+			// Retangulo ret1 = new Retangulo();
+			// ret1.DefinirMedidas(30, 30);
+			// System.Console.WriteLine($"Area: {ret1.ObterArea()}");
+
+			// Retangulo ret2 = new Retangulo();
+			// ret2.DefinirMedidas(-10, 0);
+			// System.Console.WriteLine($"Area: {ret2.ObterArea()}");
 
 			// Pessoa p1 = new Pessoa();
 
