1d825aef6940c0f1a600059d31821bdf2939d319
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-02-23 13:57:36 -0300

    aluno apresentar overrride

--- ExemploPOO/Models/Aluno.cs
+++ ExemploPOO/Models/Aluno.cs
@@ -3,5 +3,11 @@ namespace ExemploPOO.Models
 	public class Aluno : Pessoa
 	{
 		public int Nota {get; set;}
+
+		public override void Apresentar()
+		{
+			Console.WriteLine($"Olá, meu nome é {Nome} e sou aluno. Minha nota foi {Nota}.");
+		}
 	}
+	
 }
--- ExemploPOO/Models/Professor.cs
+++ ExemploPOO/Models/Professor.cs
@@ -3,5 +3,11 @@ namespace ExemploPOO
 	public class Professor : Pessoa
 	{
 		public double Salario {get; set;}
+
+		// Polimorfismo em tempo de execução
+		public override void Apresentar()
+		{
+			Console.WriteLine($"Olá, meu nome é {Nome} e sou professor. Meu salário é {Salario}.");
+		}
 	}
 }
