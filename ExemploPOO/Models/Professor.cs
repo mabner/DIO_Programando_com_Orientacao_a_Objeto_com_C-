8eb2b8cbb0976a8acb19b7fc7494491e4ca269eb
Marcos Leite <m.abner@live.co.uk>
2022-02-24 23:20:20 -0300

    sealed classes and methods

--- ExemploPOO/Models/Professor.cs
+++ ExemploPOO/Models/Professor.cs
@@ -7,7 +7,7 @@ namespace ExemploPOO
 		public double Salario {get; set;}
 
 		// Polimorfismo em tempo de execução
-		public override void Apresentar()
+		public sealed override void Apresentar()
 		{
 			Console.WriteLine($"Olá, meu nome é {Nome} e sou professor. Meu salário é {Salario}.");
 		}
