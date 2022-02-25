818c99987a3509049315cfa72b249feb02b343ac
Marcos Leite <m.abner@live.co.uk>
2022-02-25 13:36:16 -0300

    interface with methods

--- ExemploPOO/Interfaces/ICalculadora.cs
+++ ExemploPOO/Interfaces/ICalculadora.cs
@@ -10,6 +10,12 @@ namespace ExemploPOO.Interfaces
 		int Somar(int num1, int num2);
 		int Subtrair(int num1, int num2);
 		int Multiplicar(int num1, int num2);
-		int Dividir(int num1, int num2);
+
+		// Implementing the method with parameters in the interface
+		// meaning that we do not need to implement the method in the class
+		int Dividir(int num1, int num2)
+		{
+			return num1 / num2;
+		}
 	}
 }
